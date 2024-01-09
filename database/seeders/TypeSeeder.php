25edc853903f8a1635270601797c26676f322f33
ValerioCarbone <valeriocarbone10@gmail.com>
2024-01-09 18:14:58 +0100

    ADD - Seeders

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,22 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Models\Admin\Project;
+use App\Models\Admin\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 
-class ProjectSeeder extends Seeder
+class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(Faker $faker): void
     {
-        for ($i = 0; $i < 30; $i++) {
-            $new_project = new Project();
-            $new_project->title = $faker->sentence(5);
-            $new_project->save();
+        for ($i = 0; $i < 100; $i++) {
+            $new_type = new Type();
+            $new_type->name = $faker->word();
+
+            $new_type->save();
         }
     }
 }
