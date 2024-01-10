dfa2f6aa54b39b2c9f316b2270beddc3133014c2
ValerioCarbone <valeriocarbone10@gmail.com>
2024-01-10 16:53:41 +0100

    ADD - In models technologies & projects relation

--- app/Models/Admin/Technology.php
+++ app/Models/Admin/Technology.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Admin;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
